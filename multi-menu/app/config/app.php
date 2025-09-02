fed67a7aa61fdd47125f349c98011b8084c0bff0
HerickRafael <72799208+HerickRafael@users.noreply.github.com>
2025-09-02 01:38:31 -0300

    Delete multi-menu directory

--- multi-menu/app/config/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-  'app_name' => 'Multi Menu',
-  'env' => 'local',
-  'debug' => true,
-  'base_url' => 'http://localhost/multi-menu/public',
-  'session_name' => 'mm_session',
-  'timezone' => 'America/Sao_Paulo',
-];
--- multi-menu/app/config/models/Product.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/db.php';
-class Product {
-  public static function listByCompany(int $companyId): array {
-    $st = db()->prepare("SELECT * FROM products WHERE company_id = ? AND active = 1 ORDER BY sort_order, name");
-    $st->execute([$companyId]);
-    return $st->fetchAll();
-  }
-}
